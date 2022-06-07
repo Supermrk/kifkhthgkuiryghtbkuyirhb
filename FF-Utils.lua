8975123569621188645955d421c98097d9aa4be3
Supermrk <53098912+Supermrk@users.noreply.github.com>
2022-06-07 15:12:27 -0500

    Add files via upload

---
+++ FF-Utils.lua
@@ -0,0 +1,580 @@
+function getTeam(teamName)
+	if (teamName == "Cincinnati Tigers") then
+		return {
+			City = "Cincinnati",
+			Name = "Bengals",
+			EndzoneDecal = "rbxassetid://8275278583",
+			Colors = {
+				Normal = {
+					Main = "#F04A13",
+					Light = "#FF832B"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#4C4C4C"
+				},
+				Endzone = "#fb4f14"
+			},
+			TouchdownAnimation = "AFC\\AFC North"
+		}
+	elseif (teamName == "Cleveland Braves") then
+		return {
+			City = "Cleveland",
+			Name = "Browns",
+			EndzoneDecal = "rbxassetid://8539872476",
+			Colors = {
+				Normal = {
+					Main = "#FF3C00",
+					Light = "#FF6619"
+				},
+				Alternate = {
+					Main = "#472B00",
+					Light = "#5C3700"
+				},
+				Endzone = "#fb4f14"
+			},
+			TouchdownAnimation = "AFC\\AFC North"
+		}
+	elseif (teamName == "Pittsburgh Stormers") then
+		return {
+			City = "Pittsburgh",
+			Name = "Steelers",
+			EndzoneDecal = "rbxassetid://8261371661",
+			Colors = {
+				Normal = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Alternate = {
+					Main = "#DBA70B",
+					Light = "#FFC20E"
+				},
+				Endzone = "#111111"
+			},
+			TouchdownAnimation = "AFC\\AFC North"
+		}
+	elseif (teamName == "Baltimore Royals") then
+		return {
+			City = "Baltimore",
+			Name = "Ravens",
+			EndzoneDecal = "rbxassetid://8254124964",
+			Colors = {
+				Normal = {
+					Main = "#241075",
+					Light = "#2E1594"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Endzone = "#422de2"
+			},
+			TouchdownAnimation = "AFC\\AFC North"
+		}
+	elseif (teamName == "Denver Mustangs") then
+		return {
+			City = "Denver",
+			Name = "Broncos",
+			EndzoneDecal = "rbxassetid://9336227113",
+			Colors = {
+				Normal = {
+					Main = "#FB4F14",
+					Light = "#FF602B"
+				},
+				Alternate = {
+					Main = "#002B57",
+					Light = "#003870"
+				},
+				Endzone = "#003670"
+			},
+			TouchdownAnimation = "AFC\\AFC West"
+		}
+	elseif (teamName == "Kansas City Cherokees") then
+		return {
+			City = "Kansas City",
+			Name = "Chiefs",
+			EndzoneDecal = "rbxassetid://9840556899",
+			Colors = {
+				Normal = {
+					Main = "#C9243F",
+					Light = "#FF2E51"
+				},
+				Alternate = {
+					Main = "#808080",
+					Light = "#ABABAB"
+				},
+				Endzone = "#5a7d2d"
+			},
+			TouchdownAnimation = "AFC\\AFC West"
+		}
+	elseif (teamName == "Los Angeles Comets") then
+		return {
+			City = "Los Angeles",
+			Name = "Chargers",
+			EndzoneDecal = "rbxassetid://8580712259",
+			Colors = {
+				Normal = {
+					Main = "#2482D4",
+					Light = "#2B9CFF"
+				},
+				Alternate = {
+					Main = "#FDB515",
+					Light = "#FFB638"
+				},
+				Endzone = "#00aeff"
+			},
+			TouchdownAnimation = "AFC\\AFC West"
+		}
+	elseif (teamName == "Las Vegas Renegades") then
+		return {
+			City = "Las Vegas",
+			Name = "Raiders",
+			EndzoneDecal = "rbxassetid://8247748283",
+			Colors = {
+				Normal = {
+					Main = "#383838",
+					Light = "#545454"
+				},
+				Alternate = {
+					Main = "#383838",
+					Light = "#545454"
+				},
+				Endzone = "#000000"
+			},
+			TouchdownAnimation = "AFC\\AFC West"
+		}
+	elseif (teamName == "Buffalo Boars") then
+		return {
+			City = "Buffalo",
+			Name = "Bills",
+			EndzoneDecal = "rbxassetid://9501410198",
+			Colors = {
+				Normal = {
+					Main = "#002A73",
+					Light = "#003FAB"
+				},
+				Alternate = {
+					Main = "#C60C30",
+					Light = "#FF0F3F"
+				},
+				Endzone = "#003da8"
+			},
+			TouchdownAnimation = "AFC\\AFC East"
+		}
+	elseif (teamName == "Miami Sharks") then
+		return {
+			City = "Miami",
+			Name = "Dolphins",
+			EndzoneDecal = "rbxassetid://9096777627",
+			Colors = {
+				Normal = {
+					Main = "#008E97",
+					Light = "#00C8D6"
+				},
+				Alternate = {
+					Main = "#FC4C02",
+					Light = "#FF7A37"
+				},
+				Endzone = "#5a7d2d"
+			},
+			TouchdownAnimation = "AFC\\AFC East"
+		}
+	elseif (teamName == "New England Nationals") then
+		return {
+			City = "New England",
+			Name = "Patriots",
+			EndzoneDecal = "rbxassetid://8380312876",
+			Colors = {
+				Normal = {
+					Main = "#00366B",
+					Light = "#004285"
+				},
+				Alternate = {
+					Main = "#C60C30",
+					Light = "#FF2A55"
+				},
+				Endzone = "#134583"
+			},
+			TouchdownAnimation = "AFC\\AFC East"
+		}
+	elseif (teamName == "Indianapolis Cubs") then
+		return {
+			City = "Indianapolis",
+			Name = "Colts",
+			EndzoneDecal = "rbxassetid://8540182515",
+			Colors = {
+				Normal = {
+					Main = "#003C80",
+					Light = "#0052B0"
+				},
+				Alternate = {
+					Main = "#A2AAAD",
+					Light = "#B9C2C4"
+				},
+				Endzone = "#005db4"
+			},
+			TouchdownAnimation = "AFC\\AFC South"
+		}
+	elseif (teamName == "Tennessee Trojans") then
+		return {
+			City = "Tennessee",
+			Name = "Titans",
+			EndzoneDecal = "rbxassetid://9775259905",
+			Colors = {
+				Normal = {
+					Main = "#4589CC",
+					Light = "#57ABFF"
+				},
+				Alternate = {
+					Main = "#C8102E",
+					Light = "#FF2B5C"
+				},
+				Endzone = "#5a7d2d"
+			},
+			TouchdownAnimation = "AFC\\AFC South"
+		}
+	elseif (teamName == "Jacksonville Devils") then
+		return {
+			City = "Jacksonville",
+			Name = "Jaguars",
+			EndzoneDecal = "rbxassetid://9096182778",
+			Colors = {
+				Normal = {
+					Main = "#008094",
+					Light = "#0098B0"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Endzone = "#0094ab"
+			},
+			TouchdownAnimation = "AFC\\AFC South"
+		}
+	elseif (teamName == "Houston Drillers") then
+		return {
+			City = "Houston",
+			Name = "Texans",
+			EndzoneDecal = "rbxassetid://9840547501",
+			Colors = {
+				Normal = {
+					Main = "#05354C",
+					Light = "#064869"
+				},
+				Alternate = {
+					Main = "#A71930",
+					Light = "#DB2140"
+				},
+				Endzone = "#5a7d2d"
+			},
+			TouchdownAnimation = "AFC\\AFC South"
+		}
+	elseif (teamName == "Chicago Bucks") then
+		return {
+			City = "Chicago",
+			Name = "Bears",
+			EndzoneDecal = "rbxassetid://8626635590",
+			Colors = {
+				Normal = {
+					Main = "#132547",
+					Light = "#18305C"
+				},
+				Alternate = {
+					Main = "#C83803",
+					Light = "#FF4805"
+				},
+				Endzone = "#17305a"
+			},
+			TouchdownAnimation = "NFC\\NFC North"
+		}
+	elseif (teamName == "Green Bay Paladins") then
+		return {
+			City = "Green Bay",
+			Name = "Packers",
+			EndzoneDecal = "rbxassetid://9073638587",
+			Colors = {
+				Normal = {
+					Main = "#203731",
+					Light = "#355C52"
+				},
+				Alternate = {
+					Main = "#FFB612",
+					Light = "#FFCA45"
+				},
+				Endzone = "#5a7d2d"
+			},
+			TouchdownAnimation = "NFC\\NFC North"
+		}
+	elseif (teamName == "Detriot Lightning") then
+		return {
+			City = "Detriot",
+			Name = "Lions",
+			EndzoneDecal = "rbxassetid://9840529330",
+			Colors = {
+				Normal = {
+					Main = "#0076B6",
+					Light = "#009CF0"
+				},
+				Alternate = {
+					Main = "#B0B7BC",
+					Light = "#CED6DB"
+				},
+				Endzone = "#0093e2"
+			},
+			TouchdownAnimation = "NFC\\NFC North"
+		}
+	elseif (teamName == "Minnesota Vipers") then
+		return {
+			City = "Minnesota",
+			Name = "Vikings",
+			EndzoneDecal = "rbxassetid://8248769829",
+			Colors = {
+				Normal = {
+					Main = "#4F2E84",
+					Light = "#7343BF"
+				},
+				Alternate = {
+					Main = "#FFC62F",
+					Light = "#FFD562"
+				},
+				Endzone = "#7745c7"
+			},
+			TouchdownAnimation = "NFC\\NFC North"
+		}
+	elseif (teamName == "Los Angeles Rage") then
+		return {
+			City = "Los Angeles",
+			Name = "Rams",
+			EndzoneDecal = "rbxassetid://8580707336",
+			Colors = {
+				Normal = {
+					Main = "#003EB2",
+					Light = "#0054F0"
+				},
+				Alternate = {
+					Main = "#FFA300",
+					Light = "#FFC550"
+				},
+				Endzone = "#0574ea"
+			},
+			TouchdownAnimation = "NFC\\NFC West"
+		}
+	elseif (teamName == "Arizona Heat") then
+		return {
+			City = "Arizona",
+			Name = "Cardinals",
+			EndzoneDecal = "rbxassetid://9096774545",
+			Colors = {
+				Normal = {
+					Main = "#97233F",
+					Light = "#D43157"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Endzone = "#111111"
+			},
+			TouchdownAnimation = "NFC\\NFC West"
+		}
+	elseif (teamName == "San Francisco Lions") then
+		return {
+			City = "San Francisco",
+			Name = "49ers",
+			EndzoneDecal = "rbxassetid://9840600064",
+			Colors = {
+				Normal = {
+					Main = "#AA0000",
+					Light = "#E00000"
+				},
+				Alternate = {
+					Main = "#B3995D",
+					Light = "#DBBC72"
+				},
+				Endzone = "#5a7d2d"
+			},
+			TouchdownAnimation = "NFC\\NFC West"
+		}
+	elseif (teamName == "Seattle Cyclones") then
+		return {
+			City = "Seattle",
+			Name = "Seahawks",
+			EndzoneDecal = "rbxassetid://8588253026",
+			Colors = {
+				Normal = {
+					Main = "#003870",
+					Light = "#004E9C"
+				},
+				Alternate = {
+					Main = "#69BE28",
+					Light = "#84F032"
+				},
+				Endzone = "#004696"
+			},
+			TouchdownAnimation = "NFC\\NFC West"
+		}
+	elseif (teamName == "Dallas Outlaws") then
+		return {
+			City = "Dallas",
+			Name = "Cowboys",
+			EndzoneDecal = "rbxassetid://8261367689",
+			Colors = {
+				Normal = {
+					Main = "#062D63",
+					Light = "#083F8C"
+				},
+				Alternate = {
+					Main = "#869397",
+					Light = "#AFBFC4"
+				},
+				Endzone = "#1d60be"
+			},
+			TouchdownAnimation = "NFC\\NFC East"
+		}
+	elseif (teamName == "New York Greats") then
+		return {
+			City = "New York",
+			Name = "Giants",
+			EndzoneDecal = "rbxassetid://9840582925",
+			Colors = {
+				Normal = {
+					Main = "#0F2C85",
+					Light = "#143DB8"
+				},
+				Alternate = {
+					Main = "#A71930",
+					Light = "#C71E3A"
+				},
+				Endzone = "#103ab9"
+			},
+			TouchdownAnimation = "NFC\\NFC East"
+		}
+	elseif (teamName == "Philadelphia Raptors") then
+		return {
+			City = "Philadelphia",
+			Name = "Eagles",
+			EndzoneDecal = "rbxassetid://8246422323",
+			Colors = {
+				Normal = {
+					Main = "#004C54",
+					Light = "#006570"
+				},
+				Alternate = {
+					Main = "#868C8F",
+					Light = "#ADB4B8"
+				},
+				Endzone = "#008494"
+			},
+			TouchdownAnimation = "NFC\\NFC East"
+		}
+	elseif (teamName == "Washington Warriors") then
+		return {
+			City = "Washington",
+			Name = "Commanders",
+			EndzoneDecal = "rbxassetid://9840608699",
+			Colors = {
+				Normal = {
+					Main = "#5A1414",
+					Light = "#801C1C"
+				},
+				Alternate = {
+					Main = "#5A1414",
+					Light = "#801C1C"
+				},
+				Endzone = "#ffc20f"
+			},
+			TouchdownAnimation = "NFC\\NFC East"
+		}
+	elseif (teamName == "Atlanta Nighthawks") then
+		return {
+			City = "Atlanta",
+			Name = "Falcons",
+			EndzoneDecal = "rbxassetid://8733600287",
+			Colors = {
+				Normal = {
+					Main = "#A71930",
+					Light = "#D4203E"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Endzone = "#000000"
+			},
+			TouchdownAnimation = "NFC\\NFC South"
+		}
+	elseif (teamName == "New Orleans Knights") then
+		return {
+			City = "New Orleans",
+			Name = "Saints",
+			EndzoneDecal = "rbxassetid://8626631392",
+			Colors = {
+				Normal = {
+					Main = "#B29F78",
+					Light = "#F2D8A2"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Endzone = "#e8cf9c"
+			},
+			TouchdownAnimation = "NFC\\NFC South"
+		}
+	elseif (teamName == "Tampa Bay Pirates") then
+		return {
+			City = "Tampa Bay",
+			Name = "Buccaneers",
+			EndzoneDecal = "rbxassetid://8380329001",
+			Colors = {
+				Normal = {
+					Main = "#D50A0A",
+					Light = "#FF3131"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Endzone = "#111111"
+			},
+			TouchdownAnimation = "NFC\\NFC South"
+		}
+	elseif (teamName == "Carolina Predators") then
+		return {
+			City = "Carolina",
+			Name = "Panthers",
+			EndzoneDecal = "rbxassetid://9255568059",
+			Colors = {
+				Normal = {
+					Main = "#0085CA",
+					Light = "#31BEFF"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Endzone = "#111111"
+			},
+			TouchdownAnimation = "NFC\\NFC South"
+		}
+	elseif (teamName == "New York Juggernauts") then
+		return {
+			City = "New York",
+			Name = "Jets",
+			EndzoneDecal = "rbxassetid://9216711978",
+			Colors = {
+				Normal = {
+					Main = "#125740",
+					Light = "#1C8764"
+				},
+				Alternate = {
+					Main = "#292929",
+					Light = "#404040"
+				},
+				Endzone = "#006446"
+			},
+			TouchdownAnimation = "AFC\\AFC East"
+		}
+	end
+	return "None"
+end
